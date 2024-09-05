113e71a3047d4982d763707f76b1c4e6dda4a225
nodiura <nodiraura@gmail.com>
2024-09-05 13:30:37 -0400

    final successful step is done

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = file("/Users/nodiraurazbaeva/.ssh/id_ed25519.pub")
 }
 variable "prefix" {
   type    = string
