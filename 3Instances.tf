641cac8ed551c33c45398e9970ddaf10510cd02e
nodiura <nodiraura@gmail.com>
2024-09-06 12:58:26 -0400

    updated

--- 3Instances.tf
+++ 3Instances.tf
@@ -1,8 +1,4 @@
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
-}
 variable "prefix" {
   type    = string
   default = "project-aug-28"
